e96e452593ca260e00b07a8bd432f92997e824ae
Maxime-Chavigny <maxime.chavigny@viacesi.fr>
2023-06-28 14:58:12 +0200

    push
    teste

--- Front App/src/pages/Livreur.jsx
+++ Front App/src/pages/Livreur.jsx
@@ -1,8 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import CommonSection from "../components/UI/common-section/CommonSection";
 import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import "../styles/livreur.css";
+import firebase from "../firebase/config";
+import "firebase/firestore";
+
+const Tr = ({ deliveries, client }) => {
+  return (
+    <>
+      {deliveries.map((delivery) => (
+        <tr key={delivery.id}>
+          {/* Render the client and livreur data */}
+          <td className="text-center">{client.find((c) => c.id === delivery.Id_Client)?.fullName || ""}</td>
+          <td className="text-center">{delivery.Statut ? delivery.Statut : ""}</td>
+          <td className="text-center">
+            {delivery.Date_Livraison ? delivery.Date_Livraison.toDate().toLocaleString() : ""}
+          </td>
+        </tr>
+      ))}
+    </>
+  );
+};
 
 const Livreur = () => {
   const [livraisonRecuperee, setLivraisonRecuperee] = useState(false);
@@ -10,7 +30,124 @@ const Livreur = () => {
   const [accepterCommande, setAccepterCommande] = useState(false);
   const [isRecupererModalOpen, setRecupererModalOpen] = useState(false);
   const [isLivrerModalOpen, setLivrerModalOpen] = useState(false);
+  const [deliveries, setDeliveries] = useState([]);
+  const [client, setClient] = useState([]);
+  const [commandesquanti, setCommandesQuantiees] = useState([]);
+  const [foodDataArray, setFoodDataArray] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const db = firebase.firestore();
+        const snapshot = await db.collection("Commandes").where("Statut", "==", "En attente").get();
+
+        const data = snapshot.docs.map((doc) => {
+          return { id: doc.id, ...doc.data() };
+        });
+        if (data) {
+          setDeliveries(data);
+        }
+      } catch (error) {
+        console.log("Error fetching data from Firebase:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+  useEffect(() => {
+    const fetCommandesQuantités = async () => {
+      try {
+        const db = firebase.firestore();
+        const CommandePromise = deliveries.map(async (delivery) => {
+          const snapshot2 = await db
+            .collection("CommandesQuantitees")
+            .where("Id_Commande", "==", delivery.id)
+            .get();
+    
+          const commandData = snapshot2.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data()
+          }));
+    
+          return commandData;
+        });
+    
+        const commandesQuantiteesData = await Promise.all(CommandePromise);
+    
+        if (commandesQuantiteesData.length > 0) {
+          setCommandesQuantiees(commandesQuantiteesData);
+        }
+      } catch (error) {
+        console.log("Error fetching CommandesQuantitées data from Firebase:", error);
+      }
+    };
+      fetCommandesQuantités();
+      
+    },[deliveries]);
+
+
+    useEffect(() => {
+      const fetchClientData2 = async () => {
+        try {
+          if (commandesquanti !== null && commandesquanti.length > 0) {
+            const db = firebase.firestore();
+            
+            const dataPromises = commandesquanti.map(async (innerArray) => {
+              const [{ Id_Article }] = innerArray; // Destructure the inner array to get the Id_Article
+              console.log(Id_Article)
+              if (Id_Article) {
+        
+                const snapshot3 = await db.collection("Articles").doc(Id_Article).get();
+        
+        
+                const data3 = snapshot3.data();
+                
+                return data3;
+              }
+              return null; // Or handle the case where Id_Article is missing or falsy
+            });
+            
+            const articleDataArray = await Promise.all(dataPromises);
+            console.log("ArticleDataArray:", articleDataArray);
+        
+            setFoodDataArray(articleDataArray);
+          }
+        } catch (error) {
+          console.error("Error fetching ARTICLES data from Firebase:", error);
+        }
+      };
+      
+      fetchClientData2();
+    }, [commandesquanti]);
+    
+    
+  useEffect(() => {
+    const fetchClient = async () => {
+      try {
+        const db = firebase.firestore();
+        const clientPromises = deliveries.map(async (delivery) => {
+          const snapshot = await db.collection("users").doc(delivery.Id_Client).get();
+          return { id: snapshot.id, ...snapshot.data() };
+        });
+  
+        const clientData = await Promise.all(clientPromises);
+  
+        if (clientData) {
+          setClient(clientData);
+        }
+      } catch (error) {
+        console.log("Error fetching client data from Firebase:", error);
+      }
+    };
+  
+    if (deliveries.length > 0) {
+      fetchClient();
+    }
+  }, [deliveries]);
+  
+  
   const toggleRecupererModal = () => {
     setRecupererModalOpen(!isRecupererModalOpen);
   };
@@ -56,45 +193,16 @@ const Livreur = () => {
               <table className="table table-bordered">
                 <thead>
                   <tr>
-                    <th>Commande</th>
-                    <th>Date de livraison</th>
-                    <th>Nom client</th>
+                    <th>Full Name</th>
+                    <th>Statut</th>
+                    <th>Delivery Date</th>
                     <th>Action</th>
                   </tr>
                 </thead>
                 <tbody>
-                  {/* Exemple de ligne de livraison */}
-                  <tr>
-                    <td>Commande</td>
-                    <td>Date de livraison</td>
-                    <td>Nom client</td>
-                    <td>
-                      {accepterCommande ? (
-                        <>
-                          <input
-                            type="checkbox"
-                            checked={livraisonRecuperee}
-                            onChange={handleLivraisonRecupereeChange}
-                            disabled={livraisonRecuperee}
-                          />{" "}
-                          Livraison récupérée
-                          <br />
-                          <input
-                            type="checkbox"
-                            checked={livraisonLivree}
-                            onChange={handleLivraisonLivreeChange}
-                            disabled={!livraisonRecuperee || livraisonLivree}
-                          />{" "}
-                          Livrée
-                        </>
-                      ) : (
-                        <button className="btna" onClick={handleAccepterCommande}>
-                          Accepter la commande
-                        </button>
-                      )}
-                    </td>
-                  </tr>
-                  {/* Fin de l'exemple de ligne de livraison */}
+
+                  <Tr deliveries={deliveries} client={client} />
+
                 </tbody>
               </table>
             </Col>
